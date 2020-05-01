c6d6d88f2cbac8cb91e2fe2920d1446aa55dba03
3x1t1um <54857145+3x1t1um@users.noreply.github.com>
2020-05-01 12:32:07 +0200

    Create launcher.py

---
+++ src/launcher.py
@@ -0,0 +1,29 @@
+import platform
+import os
+from datetime import datetime
+
+def launch():
+	banner = """ ▄▄▄        ██████  ██░ ██  ██▓ ▄▄▄      
+▒████▄    ▒██    ▒ ▓██░ ██▒▓██▒▒████▄    
+▒██  ▀█▄  ░ ▓██▄   ▒██▀▀██░▒██▒▒██  ▀█▄  
+░██▄▄▄▄██   ▒   ██▒░▓█ ░██ ░██░░██▄▄▄▄██ 
+ ▓█   ▓██▒▒██████▒▒░▓█▒░██▓░██░ ▓█   ▓██▒
+ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░▓   ▒▒   ▓▒█░
+  ▒   ▒▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░ ▒ ░  ▒   ▒▒ ░
+  ░   ▒   ░  ░  ░   ░  ░░ ░ ▒ ░  ░   ▒   
+      ░  ░      ░   ░  ░  ░ ░        ░  ░
+                                         
+                   \033[31m{ By AskaD }\033[00m
+              [=] \033[34mAuthor\033[00m  : AskaD    [=]
+                 { \033[34mGithub\033[00m : 3x1t1um }"""
+
+	if 'Windows' in platform.platform():
+		os.system('cls')
+	else:
+		os.system('clear')
+
+	print(banner)
+
+	return datetime.now().strftime("%H:%M:%S")
+
+
